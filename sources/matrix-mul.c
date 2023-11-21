7f1d90c5d040b3360b53e3892197394764676cfa
renzhonglu11 <lu631992923q@gmail.com>
2023-11-21 01:59:25 +0100

    try to do assignment 3

--- sources/matrix-mul.c
+++ sources/matrix-mul.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define SIZE 200
+#define BLOCK_SIZE 2
 
 void printMatrix(int matrix[SIZE][SIZE]) {
     int i = 0; 
@@ -17,14 +18,16 @@ void printMatrix(int matrix[SIZE][SIZE]) {
 
 int main() {
     
-  
- 
     int matrix1[SIZE][SIZE];
     int matrix2[SIZE][SIZE];
     int result[SIZE][SIZE];
     int i = 0;
     int j = 0;
     int k = 0;
+    int ii = 0;
+    int jj = 0; 
+    int kk = 0; 
+
 
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
@@ -33,19 +36,48 @@ int main() {
         }
     }
     
-
-    
-
     for (i = 0; i < SIZE; i++) {
     for (j = 0; j < SIZE; j++) {
         result[i][j] = 0;
-        
-        for (k = 0; k < SIZE; k++) {
-            result[i][j] += matrix1[i][k] * matrix2[k][j];
+    }
+}
+
+    
+for (ii = 0; ii < SIZE; ii += BLOCK_SIZE) {
+    for (jj = 0; jj < SIZE; jj += BLOCK_SIZE) {
+        for (kk = 0; kk < SIZE; kk += BLOCK_SIZE) {
+           
+            for (i = ii; i < ii + BLOCK_SIZE; i++) {
+                for (j = jj; j < jj + BLOCK_SIZE; j++) {
+            
+                    for (k = kk; k < kk + BLOCK_SIZE; k++) {
+                        
+                        result[i][j] += matrix1[i][k] * matrix2[k][j];
+                    }
+                    
+                    
+                }
+            }
         }
     }
-    }
+}
+
  
 
+
+
+#if 0
+
+        for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            result[i][j] = 0;
+            
+            for (k = 0; k < SIZE; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+        }
+ 
+  #endif
     return 0;
 }
